fa30667eef61c86dbe61dd41950cfde6b2c83ba7
sinangabel <sinan.gabel@gmail.com>
2017-06-21 14:06:19 +0200

    more aiUpdown() test examples

--- tests/ai_curl.sh
+++ tests/ai_curl.sh
@@ -1,2 +1,4 @@
 
 curl -H "Content-Type: application/json" -d "@ai_data.json" -X POST  https://riskbutler.net/ocpu/library/riskbutlerRadar/R/aiUpdown/json
+
+curl -H "Content-Type: application/json" -d "@ai_data2.json" -X POST  https://riskbutler.net/ocpu/library/riskbutlerRadar/R/aiUpdown/json
